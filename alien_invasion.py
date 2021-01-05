eacaf8ed23817d44bd5af0e6701c839b7d323901
zhoulichao <zhoulc1220@thundersoft.com>
2021-01-05 14:57:23 +0800

    Adjust the ship speed and restrict the area

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #background color
     bg_color = (230, 230, 230)
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
         gf.check_events(ship)
--- ship.py
+++ ship.py
@@ -1,7 +1,8 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
+        self.ai_settings = ai_settings
         self.screen = screen
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -9,6 +10,7 @@ class Ship():
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
 
         self.moving_right = False
         self.moving_left = False
@@ -17,7 +19,9 @@ class Ship():
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
