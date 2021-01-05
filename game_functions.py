a96533b8607d7a9d9cb3f57e010424deff30265e
zhoulichao <zhoulc1220@thundersoft.com>
2021-01-05 14:20:35 +0800

    press left or right to move the ship

--- game_functions.py
+++ game_functions.py
@@ -7,7 +7,14 @@ def check_events(ship):
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
--- ship.py
+++ ship.py
@@ -11,6 +11,7 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
         self.moving_right = False
+        self.moving_left = False
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
@@ -18,3 +19,5 @@ class Ship():
     def update(self):
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -=1
